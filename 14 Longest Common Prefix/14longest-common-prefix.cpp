edc1d48b51b3d78ef30a2ca55c429945881ff634
AdityaMane231105 <adityamane104798@gmail.com>
2025-11-01 09:13:11 +0000

    Code challenge submission for question: 14 Longest Common Prefix

---
+++ 14 Longest Common Prefix/14longest-common-prefix.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        // Start with the first string as the prefix
+        string prefix = strs[0];
+
+        // Compare with each string in the list
+        for (int i = 1; i < strs.size(); i++) {
+            // Shorten prefix until it matches the start of current string
+            while (strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.size() - 1);
+                if (prefix.empty()) return "";
+            }
+        }
+        return prefix;
+    }
+};
