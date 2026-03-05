0b6dc7615e760be45541c36a5298a3c7d67b5179
AdityaMane231105 <adityamane104798@gmail.com>
2026-03-05 05:29:55 +0000

    Code challenge submission for question: 81 Search in Rotated Sorted Array II

---
+++ 81 Search in Rotated Sorted Array II/81search-in-rotated-sorted-array-ii.c
@@ -0,0 +1,33 @@
+bool search(int* nums, int numsSize, int target) {
+    int low = 0, high = numsSize - 1;
+    
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        
+        if (nums[mid] == target) return true;
+        
+        // Handle duplicates: if ends and mid are the same, we can't tell which side is sorted
+        if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+            low++;
+            high--;
+        } 
+        // Left side is sorted
+        else if (nums[low] <= nums[mid]) {
+            if (target >= nums[low] && target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        } 
+        // Right side is sorted
+        else {
+            if (target > nums[mid] && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+    
+    return false;
+}
