47f0a8c8261f064b3d64ad0aa7e2427121c0aa5b
AdityaMane231105 <adityamane104798@gmail.com>
2026-03-24 10:33:02 +0000

    Code challenge submission for question: 190 Reverse Bits

---
+++ 190 Reverse Bits/190reverse-bits.c
@@ -0,0 +1,12 @@
+uint32_t reverseBits(uint32_t n) {
+    uint32_t result = 0;
+    for (int i = 0; i < 32; i++) {
+        // Shift result left to make room for the next bit
+        result <<= 1;
+        // Extract the last bit of n and add it to result
+        result |= (n & 1);
+        // Shift n right to process the next bit
+        n >>= 1;
+    }
+    return result;
+}
